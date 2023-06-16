673de38eea5eb5ecb776ed6881922924ded33b2d
Ruslan <48005934+zluxy@users.noreply.github.com>
2023-06-16 19:05:12 +0500

    Add files via upload

---
+++ Models/Data.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZluxiaWeather.Models
+{
+    public class Data
+    {
+        public w[] weather { get; set; }
+        public Model main { get; set; }
+        public int visibility { get; set; }
+        public Model wind { get; set; }
+        public Model clouds { get; set; }
+
+        public Model rain { get; set; }
+        public Model snow { get; set; }
+
+        public int dt { get; set; }
+
+        public Model sys { get; set; }
+
+        public int timezone { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+
+    public class w
+    {
+        #region weather
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+        #endregion
+    }
+}
